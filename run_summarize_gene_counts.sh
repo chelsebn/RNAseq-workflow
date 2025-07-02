e50a1674bb085b2936b98e5f92ab31c535dc3bd6
chelsebn <chelsebn@uci.edu>
2025-07-02 10:20:18 -0700

    Update run_summarize_gene_counts.sh

--- run_summarize_gene_counts.sh
+++ run_summarize_gene_counts.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#SBATCH --job-name=summarize_gene_counts    ## Name of the job.
+#SBATCH -A  CHELSEBN 
+#SBATCH -p free              ## partition name
+#SBATCH --nodes=1             ## (-N) number of nodes to use
+#SBATCH --ntasks=1           ## (-n) number of tasks to launch
+#SBATCH --cpus-per-task=4     ## number of cores the job needs
+#SBATCH --error=summarize_gene_counts-%J.err  ## error log file
+#SBATCH --output=summarize_gene_counts-%J.out ## output log file
+
 # Move to the folder containing the BAM files
-cd results/3_aligned_sequences/aligned_bam || {
-  echo "Error: cannot cd into results/3_aligned_sequences/aligned_bam"
+cd /pub/chelsebn/BakerLab/RNAseq_data/results/3_aligned_sequences/aligned_bam || {
+  echo "Error: cannot cd into /pub/chelsebn/BakerLab/RNAseq_data/results/3_aligned_sequences/aligned_bam"
   exit 1
 }
 
@@ -11,14 +20,11 @@ dirlist=$(ls -t ./*.bam | tr '\n' ' ')
 echo "Processing the following BAM files:"
 echo "$dirlist"
 
-# Create output directory if it does not exist
-mkdir -p ../../results/4_final_counts
-
 # Run featureCounts
 featureCounts \
-  -a ../../annotation/* \
-  -o ../../results/4_final_counts/final_counts.txt \
-  -g gene_name \
+  -a /pub/chelsebn/BakerLab/RNAseq_data/annotation/* \
+  -o /pub/chelsebn/BakerLab/RNAseq_data/results/4_final_counts/final_counts.txt \
+  -g gene_id \
   -T 4 \
   -M \
   --fraction \
